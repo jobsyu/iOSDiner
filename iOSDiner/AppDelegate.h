3883c71ca8a07fc811213ae4cae45f776e12e796
於程鹏 <yuchengpeng@yuchengpengdeMac.local>
2015-03-31 06:17:02 -0700

    Initial Commit

---
+++ iOSDiner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSDiner
+//
+//  Created by 於程鹏 on 15-3-31.
+//  Copyright (c) 2015年 於程鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSDiner/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOSDiner
+//
+//  Created by 於程鹏 on 15-3-31.
+//  Copyright (c) 2015年 於程鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
